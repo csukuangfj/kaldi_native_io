d79883c411749456535030f51dcca4ad15aafd60
Fangjun Kuang <csukuangfj@gmail.com>
2022-02-01 18:36:41 +0800

    Add int32_pair_vector type. (#4)

--- kaldi_native_io/python/kaldi_native_io/__init__.py
+++ kaldi_native_io/python/kaldi_native_io/__init__.py
@@ -1,10 +1,13 @@
 from .table_types import (
+    Int32PairVectorWriter,
     Int32VectorVectorWriter,
     Int32VectorWriter,
     Int32Writer,
+    RandomAccessInt32PairVectorReader,
     RandomAccessInt32Reader,
     RandomAccessInt32VectorReader,
     RandomAccessInt32VectorVectorReader,
+    SequentialInt32PairVectorReader,
     SequentialInt32Reader,
     SequentialInt32VectorReader,
     SequentialInt32VectorVectorReader,
