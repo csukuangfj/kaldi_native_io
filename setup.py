c54c15d3bc2e5c026065c2be38cc0f4c068fa075
Fangjun Kuang <csukuangfj@gmail.com>
2022-08-14 22:13:41 +0800

    Support make install (#33)

--- setup.py
+++ setup.py
@@ -4,14 +4,20 @@
 
 import glob
 import os
+import platform
 import re
 import shutil
 import sys
+from pathlib import Path
 
 import setuptools
 from setuptools.command.build_ext import build_ext
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def is_windows():
+    return platform.system() == "Windows"
 
 
 def cmake_extension(name, *args, **kwargs) -> setuptools.Extension:
@@ -37,6 +43,9 @@ class BuildExtension(build_ext):
         if cmake_args == "":
             cmake_args = "-DCMAKE_BUILD_TYPE=Release"
 
+        extra_cmake_args = " -DKALDI_NATIVE_IO_BUILD_TESTS=OFF "
+        extra_cmake_args += f" -DCMAKE_INSTALL_PREFIX={Path(self.build_lib).resolve()}/kaldi_native_io "  # noqa
+
         if make_args == "" and system_make_args == "":
             print("For fast compilation, run:")
             print(
@@ -48,34 +57,52 @@ class BuildExtension(build_ext):
             print(f"Setting PYTHON_EXECUTABLE to {sys.executable}")
             cmake_args += f" -DPYTHON_EXECUTABLE={sys.executable}"
 
-        build_cmd = f"""
-            cd {self.build_temp}
+        cmake_args += extra_cmake_args
 
-            cmake {cmake_args} {kaldi_native_io_dir}
-
-            make {make_args} _kaldi_native_io
-        """
-        print(f"build command is:\n{build_cmd}")
-
-        ret = os.system(build_cmd)
-        if ret != 0:
-            raise Exception(
-                "\nBuild kaldi_native_io failed. Please check the error "
-                "message.\n"
-                "You can ask for help by creating an issue on GitHub.\n"
-                "\nClick:\n"
-                "   https://github.com/csukuangfj/kaldi_native_io/issues/new\n"
+        if is_windows():
+            build_cmd = f"""
+                cmake {cmake_args} -B {self.build_temp} -S {kaldi_native_io_dir}
+                cmake --build {self.build_temp} --target _kaldi_native_io --config Release -- -m
+                cmake --build {self.build_temp} --target install --config Release -- -m
+            """
+            print(f"build command is:\n{build_cmd}")
+            ret = os.system(
+                f"cmake {cmake_args} -B {self.build_temp} -S {kaldi_native_io_dir}"
             )
-        lib_so = glob.glob(f"{build_dir}/lib/*.so*")
-        for so in lib_so:
-            print(f"Copying {so} to {self.build_lib}/")
-            shutil.copy(f"{so}", f"{self.build_lib}/")
 
-        # macos
-        lib_so = glob.glob(f"{build_dir}/lib/*.dylib*")
-        for so in lib_so:
-            print(f"Copying {so} to {self.build_lib}/")
-            shutil.copy(f"{so}", f"{self.build_lib}/")
+            if ret != 0:
+                raise Exception("Failed to configure kaldi_native_io")
+
+            ret = os.system(
+                f"cmake --build {self.build_temp} --target _kaldi_native_io --config Release -- -m"
+            )
+            if ret != 0:
+                raise Exception("Failed to build kaldi_native_io")
+
+            ret = os.system(
+                f"cmake --build {self.build_temp} --target install --config Release -- -m"
+            )
+            if ret != 0:
+                raise Exception("Failed to install kaldi_native_io")
+        else:
+            build_cmd = f"""
+                cd {self.build_temp}
+
+                cmake {cmake_args} {kaldi_native_io_dir}
+
+                make {make_args} _kaldi_native_io install
+            """
+            print(f"build command is:\n{build_cmd}")
+
+            ret = os.system(build_cmd)
+            if ret != 0:
+                raise Exception(
+                    "\nBuild kaldi_native_io failed. Please check the error "
+                    "message.\n"
+                    "You can ask for help by creating an issue on GitHub.\n"
+                    "\nClick:\n"
+                    "   https://github.com/csukuangfj/kaldi_native_io/issues/new\n"
+                )
 
 
 def read_long_description():
